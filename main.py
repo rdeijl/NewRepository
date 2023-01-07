c66e17f3d99e52949891afa50ee0f81c11e7a094
rdeijl <deijlrobin@gmail.com>
2023-01-07 21:10:32 +0100

    updated code

--- main.py
+++ main.py
@@ -1,4 +1,21 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def create_dataset():
+
+
+def main():
+    print("Hello World!")
 
 
 if __name__ == "__main__":
-    print("Hello World!")
+    main()
+
+
+# Retreive and format data
+#
+# Build signals
+#
+# Backtest
+#
+# Trade
