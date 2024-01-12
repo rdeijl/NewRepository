8472df78524d9a736fd4dac6e9726c540c40eef8
rdeijl <deijlrobin@gmail.com>
2024-01-12 21:49:56 +0100

    Restarted code base

--- main.py
+++ main.py
@@ -1,21 +1,3 @@
-import numpy as np
-import matplotlib.pyplot as plt
-
-def create_dataset():
 
 
-def main():
-    print("Hello World!")
-
-
-if __name__ == "__main__":
-    main()
-
-
-# Retreive and format data
-#
-# Build signals
-#
-# Backtest
-#
-# Trade
+print("Hello world")
